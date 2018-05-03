c05995b5798260f6153652dd5c1874d9750a1fd8
Joseph Kim <josephikim2@gmail.com>
2018-05-03 10:57:48 -0700

    resync

--- javascript/keys.js
+++ javascript/keys.js
@@ -7,8 +7,12 @@ exports.twitter = {
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
+// exports.spotify = {  // From David
+//   id: '34e84d93de6a4650815e5420e0361fd3',
+//   secret: '5162cd8b5cf940f48702dffe096c2acb'
+// };
+
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
--- javascript/liri.js
+++ javascript/liri.js
@@ -1,40 +1,100 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
+
+
 
 // include packages  
-require("dotenv").config();
+
+var dotenv = require("dotenv").config();
+var inquirer = require('inquirer')
+
+var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
 var fs = require('fs');
-var keys = require('./keys.js');
+
 
 
 console.log('keys: ', keys)
-
+// console.log('process.env: ', process.env)
 // create app clients
 var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
  
-spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
+// // This code works
+// spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+//     if ( err ) {
+//         console.log('Error occurred: ' + err);
+//         return;
+//     }
  
-  response = JSON.stringify(data)
-  console.log(response)
-});
+//   response = JSON.stringify(data)
+//   console.log(response)
+// });
 
 // Takes in all of the command line arguments
 var inputString = process.argv;
 
-// Parses the command line argument to capture the "operand" (add, subtract, multiply, etc) and the numbers
+// Parses the command line argument to capture optional arguments
 var command = inputString[2];
-var arg1 = inputString[3];
+// var arg1 = inputString[3];
+// console.log('command: ', command)
+// console.log('arg1: ', arg1)
+if(command === 'my-tweets') {
+  // client.get('statuses/lookup', 3, function (error, response, body) {
+  //   console.log('error:', error); // Print the error if one occurred
+  //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //   console.log('body:', body); // Print the HTML for the Google homepage.
+  // });
+  var params = {screen_name: 'real_joeyk', count: 3};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      for (var key in tweets) {
+        var text = tweets[key].text;
+        // var volume = jsonResponse.trends[key].tweet_volume;
+        console.log(key, "text - " + text);
+      }
+      
+      // console.log('response' + console.dir(response));
+      // console.log(typeof response)
 
+    }
+  });
+  // client.get('https://api.twitter.com/1.1/search/tweets.json', {q: 'from:name param',count: "3"},  function(error, tweets, response){
+  //   if(error) {
+  //     throw error;
+  //   } else {
+  //     console.log(tweets);  
+  //   }
+  // });
+  // client.twitter(response) {
+  //   show your last 20 tweets
+  //   show when they were created
+  // }
 
-console.log('command: ', command)
-console.log('arg1: ', arg1)
+  // console.log(20 tweets)
+}
+
+// inquirer.prompt([
+
+//   {
+//     type: "input",
+//     name: "userInput",
+//     message: "Which location or landmark would you like to geocode?"
+//   }
+
+// // After the prompt, store the user's response in a variable called location.
+// ]).then(function(location) {
+
+//   // console.log(location.userInput);
+
+//   // Then use the Google Geocoder to Geocode the address
+//   // geocoder.geocode(location.userInput, function(err, data) {
+
+//   //   console.log(JSON.stringify(data, null, 2));
+//   // });
+
+// });
 
 
 // // Here's the variable we will be modifying with the new numbers
